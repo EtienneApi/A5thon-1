4549c714dc8913b418e04a3fbf97619bf6163fe3
EtienneApi <93768053+EtienneApi@users.noreply.github.com>
2022-05-11 18:23:00 +0200

    mise en place d'une structure commune

---
+++ frontend/src/pages/Adultes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Adultes() {
+  return (
+    <div>
+      <h1>Adultes</h1>
+    </div>
+  );
+}
+
+export default Adultes;
---
+++ frontend/src/pages/Enfants.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Enfants() {
+  return (
+    <div>
+      <h1>Enfants</h1>
+    </div>
+  );
+}
+
+export default Enfants;
