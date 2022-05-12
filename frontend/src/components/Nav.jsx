3edaa4b3d7b8d0d01aa9f378fcd00bb6166bea91
Mariakarara <claind15@yahoo.fr>
2022-05-12 21:35:08 +0200

    travail header/footer

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Nav() {
-  return <div>Nav</div>;
+function Nav(handleShowMenu) {
+  return (
+    <ul>
+      <NavLink to="/" onClick={() => handleShowMenu()}>
+        <li>Accueil</li>
+      </NavLink>
+      <NavLink to="/eco-quiz" onClick={() => handleShowMenu()}>
+        <li>Eco-quiz</li>
+      </NavLink>
+      <NavLink to="/adotion" onClick={() => handleShowMenu()}>
+        <li>Adopte un arbre</li>
+      </NavLink>
+      <NavLink to="/infos" onClick={() => handleShowMenu()}>
+        <li>Infos</li>
+      </NavLink>
+      <NavLink to="/engagement" onClick={() => handleShowMenu()}>
+        <li>Engagement</li>
+      </NavLink>
+
+      <div className="img1-responsive-menu " />
+    </ul>
+  );
 }
 
 export default Nav;
