e7fcf54d41457dee8ad2475919b1361638d30db4
EtienneApi <93768053+EtienneApi@users.noreply.github.com>
2022-05-13 09:24:49 +0200

    Résolution du problème des cartes sur Tree

--- frontend/src/components/Tree.jsx
+++ frontend/src/components/Tree.jsx
@@ -5,7 +5,7 @@ import pictures from "../data/Pictures";
 function Tree({ tree, index }) {
   return (
     <div className="cardsAdoption">
-      <h1>{tree.name}</h1>
+      <h2>{tree.name}</h2>
       <img className="picture" src={pictures[index].image} alt={tree.name} />
       <button type="button" className="buttonAdoption">
         Je veux adopter un {tree.name} pour {tree.price}€/mois
